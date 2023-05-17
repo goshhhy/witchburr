4c7e1420d19e8a0d807d62825bde1c2b33e29943
linear cannon <dev@linear.network>
2023-05-17 16:36:24 -0400

    add empty reg.hpp

---
+++ include/witchburr/reg.hpp
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: Copyright (c) 2023 linear cannon <dev@linear.network>
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include "witchburr/witchburr_exception.hpp"
+
+namespace witchburr {
+struct Reg;
+
+
+}
